6f3c7cf30ca419bb47b16221b47ca99789092b57
Vincenzo FOTI <vincenzo.foti@outlook.it>
2023-04-28 16:06:19 +0200

    add dynamic Elements

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public string Image { get; set; }
+        public double Price { get; set; }
+
+        public Pizza(string nome, string descrizione, string image, double price)
+        {
+            Nome = nome;
+            Descrizione = descrizione;
+            Image = image;
+            Price = price;
+        }
+    }
+}
